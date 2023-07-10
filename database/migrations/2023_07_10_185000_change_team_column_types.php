52d6f448c66f9de639c062510cd16299610ff891
yahyawick <ya1yah2@gmail.com>
2023-07-10 11:58:30 -0700

    basic team table set

---
+++ database/migrations/2023_07_10_185000_change_team_column_types.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('logo')->nullable()->change();
+            $table->string('color')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('logo')->nullable(false)->change();
+            $table->string('color')->nullable(false)->change();
+        });
+    }
+};
